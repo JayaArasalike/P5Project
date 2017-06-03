3135bdba5013bbe69e529191fee7431e92eee2de
Jayalaxmi Arasalike <jayalaxmiarasalike@Jayalaxmis-MacBook-Pro.local>
2017-06-03 11:16:55 -0700

    added the <img> to display users profile pic, and hide page 3

--- firebaseP5JSsketch.js
+++ firebaseP5JSsketch.js
@@ -19,6 +19,7 @@ var id;
 var startTime;
 var endTime;
 var duration;
+var profilePic;
 
 //run this funciton, only after the document loads
 $(document).ready(function() {
@@ -26,16 +27,17 @@ $(document).ready(function() {
 
   //initialize the score and set the initail score on the webpage
   function init(){
-  score = 0;
-  $("#i2").html(score);
+      score = 0;
+      $("#i2").html(score);
   }
 
+  //call the init() function to initialize score to zero
   init();
   
 
 
 //on clicking this button score increases
-  $("#bt1").on("click", function(){
+  $("#bt1").on("click", function() {
       
       console.log("Score before increment: ", score);
       $("#i2").html(++score);
@@ -56,7 +58,7 @@ $(document).ready(function() {
       console.log("Duration is: ", duration);
 
       
-
+      //creating an object to hold the data, which will be sent to firebase 
       var data = {
         name: $("#name").val(),
         memberId: id,
@@ -70,42 +72,44 @@ $(document).ready(function() {
       usersRef.push(data);
 
       usersRef.orderByChild("memberId").equalTo(id).on("child_added", function(snapshot) {
-        console.log(snapshot.val());
+      console.log(snapshot.val());
 
-        score = snapshot.val().score;
-        duration = snapshot.val().duration;
-        testDate = snapshot.val().testDate;
+      //get the snapshot of user's score, duration and testDate based on member id
+      score = snapshot.val().score;
+      duration = snapshot.val().duration;
+      testDate = snapshot.val().testDate;
 
-        // Add each train's data into the table
-        $("#score-table > tbody").append("<tr><td>" + score+ "</td><td>" + duration + "</td><td>" +
-          testDate + "</td></tr>");
+      // Add user's score data into the table
+      $("#score-table > tbody").append("<tr><td>" + score+ "</td><td>" + duration + "</td><td>" +
+      testDate + "</td></tr>");
       });
 
   });
 
-  
 
 }); //end of document.ready()
 
 
-
-
-
 //on start button click, display page2
-  function page2Handler(){
+function page2Handler(){
     startTime = moment();
     console.log("Start time :",startTime);
     console.log("Inside page2Handeler");
     $("#page1").css({ visibility: "hidden"}); 
     $("#page2").css({ visibility: "visible"});
-  }
+    
+    //creating an image tag to hold and display user's profile pic
+    profilePic = $('<img>');
+    profilePic.attr("src", photo);
+    pic.append(profilePic);
+}
 
 //on start button click, display page2
-  function page3Handler(){
+function page3Handler(){
     console.log("Inside page2Handeler");
     $("#page2").css({ visibility: "hidden"}); 
     $("#page3").css({ visibility: "visible"});
-  }
+}
 
 //linkedIn functions, attaching auth eventhandler
 function OnLinkedInFrameworkLoad() {
@@ -119,7 +123,6 @@ function OnLinkedInAuth() {
 
 function getProfileData(profiles) {
 
-
     var member = profiles.values[0];
     id = member.id;
     console.log('set id', id);
@@ -128,18 +131,6 @@ function getProfileData(profiles) {
     var lastName = member.lastName; 
     var photo = member.pictureUrl; 
     var headline = member.headline; 
-
-    /*var user = {
-      fName: firstName,
-      lName: lastName,
-      scores: []
-    };
-    
-    
-    usersRef.push(id);
-    userRef = database.ref(('/' + id));
-    userRef.push(user);*/
-
     
     //use information captured above
     console.log("First name:", firstName);
